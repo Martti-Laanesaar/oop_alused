0f2e73f70919a7c8573947145ff1a545c944bb0e
Martti Laanesaar <laanesaar.martti@khk.ee>
2021-02-18 13:05:01 +0200

    Ülesanne 4.1 - inimene klass ja testimine

---
+++ 3.teema/test.py
@@ -0,0 +1,9 @@
+from inimene import Inimene
+#Loome testimiseks 3 eksemplari
+inimene1 = Inimene()
+inimene2 = Inimene()
+inimene3 = Inimene()
+#väljastame nende id-d
+inimene1.info()
+inimene2.info()
+inimene3.info()
